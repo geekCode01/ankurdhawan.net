2cbf243714487d2e5cd8831d726b2bff911c1a60
Ankur Dhawan <ankurdhawan504@gmail.com>
2020-05-23 15:29:09 +0530

    About me

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Ankur Dhawan`,
     siteUrl: `https://ankurdhawan.net`,
-    description: `A Gatsby starter for nehalem`,
+    description: `Welcome to my blog.`,
     topics: [],
     menu: [
       {
@@ -18,16 +18,15 @@ module.exports = {
     search: true,
     author: {
       name: `ankurdhawan`,
-      description: `I'm <strong>nehalem</strong>, a Gatsby theme by
-        <a href="https://nehalist.io" rel="noopener" target="_blank">nehalist.io</a>. If you like what you see feel free to give a
-        <a href="https://github.com/nehalist/gatsby-theme-nehalem" rel="noopener" target="_blank">star on GitHub!</a>`,
+      description: `I'm <strong>Ankur</strong>.
+         If you like what you see feel free to give a <a href="https://github.com/ankur786-coder/ankurdhawan.net" rel="noopener" target="_blank">star on GitHub!</a>`,
       social: {
         facebook: ``,
-        twitter: `https://twitter.com/nehalist`,
-        linkedin: `https://www.linkedin.com/in/kevin-hirczy-7a9377106/`,
+        twitter: `https://twitter.com/AnkurDh86416728`,
+        linkedin: `https://www.linkedin.com/in/ankur-dhawan-5494151a8`,
         instagram: ``,
         youtube: ``,
-        github: `https://github.com/nehalist`,
+        github: `https://github.com/ankur786-coder`,
         twitch: ``
       }
     }
@@ -40,8 +39,8 @@ module.exports = {
           name: `nehalem - A Gatsby theme`,
           short_name: `nehalem`,
           start_url: `/`,
-          background_color: `#a4cbb8`,
-          theme_color: `#a4cbb8`,
+          background_color: `#000000`,
+          theme_color: `#000000`,
           display: `minimal-ui`,
           icon: `${__dirname}/content/assets/images/logo.png`
         }
